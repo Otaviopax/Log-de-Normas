965e9456b247213a4372ab1abec59e9b87a168ce
otaviopax <otaviopax@gmail.com>
2026-05-25 09:31:36 -0300

    feat: Criação de Rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Formulario from '../views/Formulario.vue'
+import Historico from '../views/Historico.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/pesquisa/:norma',
+      name: 'Pesquisa Normas',
+      props: true,
+      component: Formulario
+    },
+    {
+      path: '/historico/:id',
+      name: 'event-list',
+      props: true,
+      component: Historico
+    },
+  ],
 })
 
 export default router
